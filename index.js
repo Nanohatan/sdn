e063813ad486f4b4d8ad3d4e96ac5600b55245d4
kanon <yuriika.0000@gmail.com>
2020-10-23 22:31:41 +0900

    現時点のものをmergeしたやつです

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var fs = require('fs');
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 
+const multer  = require('multer');
+var busboy = require("connect-busboy");
+const path = require("path");
+// const Makedir = require("make-dir");
+const { chdir } = require('process');
+let execSync = require('child_process').execSync;
+app.use(express.json());
+
+
 
 app.set('view engine', 'ejs');
 
@@ -58,6 +67,61 @@ io.sockets.on("connection", function(socket){
 
 
 	});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // console.log(typeof(JSON.stringify(file.originalname)));
+    var dir = 'uploads/' + JSON.parse(JSON.stringify(file)).originalname.split(".")[0];
+    // var dir = 'uploads/' + JSON.stringify(file.originalname);
+    var fs = require('fs');
+    if (!fs.existsSync(dir)) {
+      //create dir
+      var command = "mkdir -p "+dir;
+      execSync(command);
+      // fs.mkdir(dir);
+      // Makedir(dir);
+    };
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+      console.log(file);
+      cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage: storage});
+
+app.use('/upload/', function(req, res, next) {
+  res.sendfile('static/upload.html'); // the uploaded file object
+  // res.send('<form action="/upload" method="POST" enctype="multipart/form-data">'+
+  // '<input name="movie" type="file"/>'+
+  // '<input type="submit" name="sub_buttono" value="Upload">'+'</form>');
+  console.log("get upload");
+});
+
+app.post('/upload', upload.single('movie'), function (req, res, next) {
+  console.log('post upload');
+  console.log('req filepath:', req.file.path)
+  // var req = JSON.stringify(req);
+  // console.log('req.file:',req);
+  createSegments(__dirname, req);
+  console.log('createSegments END');
+  // console.log(`req.body: ${JSON.stringify(req.file)}`);
+  res.redirect("/upload");
+});
+
+
+
+//functions
+function createSegments(dir_name, req){
+  console.log('createSegments START');
+  var f_dir = dir_name + "/" + req.file.destination + "/";
+  var f_in_path = f_dir + req.file.filename;
+  var f_out_dir = f_dir + 'video%3d.ts" ' + f_dir + 'playlist.m3u8';
+  var command = 'ffmpeg -i ' + f_in_path + ' -codec copy -bsf:v h264_mp4toannexb -hls_time 9 -loglevel error -hls_list_size 0 -hls_segment_filename "'+ f_out_dir;
+  var result =  execSync(command);
+  // return 1
+};
+
 
 
 //app.listen(8080)
