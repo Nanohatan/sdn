2f07466eae0e624450f367ca7c9dfed6721627ef
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-12-15 11:51:01 +0000

    ユーザー情報をcookieから撮ってページを作成するようにした

--- auth.js
+++ auth.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-var router = express.Router();
+var router = express.Router()
+
+//mongodb
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 // create application/json parser
 var jsonParser = bodyParser.json()
@@ -17,7 +21,24 @@ router.get('/login', function(req, res) {
   if (req.cookies.uid==null){
     res.redirect('/auth')
   }else{
-    res.render("userPage.ejs", {"role": "role set here","userName":"user name here"})
+    var n,r;
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("userInfo");
+        dbo.collection("users").findOne({uid:req.cookies.uid}, function(err, result) {
+	  if (err) throw err;
+          if (result==undefined){
+            res.redirect('/auth')
+          }else {
+
+          n=result.name;
+          r=result.isTeacher;
+	  console.log(result.name);
+	  db.close();
+          res.render("userPage.ejs", {"role": r,"userName":n})}
+        });
+    });
+    //res.render("userPage.ejs", {"role": r,"userName":n})
   }
 });
 
