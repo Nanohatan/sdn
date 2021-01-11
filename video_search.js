905210b54e42063e32d90caeb32437471d2edc22
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2021-01-11 05:30:19 +0000

    動画検索画面に、動画一覧を表示するように変更

--- video_search.js
+++ video_search.js
@@ -2,9 +2,20 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var router = express.Router()
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 router.get('/', function(req, res) {
-  res.sendfile('static/video_search.html');
+  //res.sendfile('static/video_search.html');
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("movieInfo");
+  dbo.collection("movies").find({}, { projection: { _id: 0} }).limit(5).toArray(function(err, result) {
+    if (err) throw err;
+    db.close();
+    res.render("video_search", {"jd":result})
+  });
+});
 });
 
 module.exports = router;
