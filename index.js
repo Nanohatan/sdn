a2a83043d38ad533687423b26d48e2705f452bb9
kanon <yuriika.0000@gmail.com>
2021-01-12 16:01:18 +0900

    no message

--- index.js
+++ index.js
@@ -5,22 +5,20 @@ var fs = require('fs');
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 
-const multer  = require('multer');
+const multer = require('multer');
 var busboy = require("connect-busboy");
 const path = require("path");
-// const Makedir = require("make-dir");
-const { chdir } = require('process');
+const {chdir} = require('process');
 let execSync = require('child_process').execSync;
-//app.use(express.json());
 
 //using auth
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-app.use('/auth',express.static('static/auth'))
+app.use('/auth', express.static('static/auth'))
 var auth = require('./auth.js')
 app.use('/auth', auth);
-app.use('/search',require('./video_search.js'))
-app.use('/movie_upload',require('./movie_upload.js'))
+app.use('/search', require('./video_search.js'))
+app.use('/movie_upload', require('./movie_upload.js'))
 
 var Puid = require('puid');
 var puid;
@@ -45,7 +43,6 @@ app.use('/uploads', express.static('uploads'));
 memers = []; //users array
 memeConnections = []; //connections array
 server.listen(process.env.PORT || 8080);
-console.log("Meme Server Is Up"); 
 
 app.get('/', function (req, res) {
     //res.render('index');
@@ -57,149 +54,132 @@ app.get('/test', function (req, res) {
 });
 
 app.get('/video/:id', function (req, res) {
-    var sorce_url = "/uploads/"+req.params.id+"/playlist.m3u8";
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("movieInfo");
-  var query = { movie_id: req.params.id };
-  dbo.collection("movies").find(query).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-    res.render('class_page',{role:false,className:result[0].lecture_name,videoTitle:result[0].lecture_name,videoNumber:result[0].lecture_time, place:sorce_url});
-  });
-});
-});
-
-
-//app.get('/:id', function (req, res) {
-//    res.send('respond user Info userid:' + req.params.id);
-//});
-
-app.get('/userlist', function(req, res) {
-    MongoClient.connect(url, connectOption, function(err, client) {
-        const col = client.db(dbName).collection('movies');
-	col.find().toArray(function(err, items) {
-	    console.log(url);
-            res.render('videolist', {
-                "userlist" : items
+    var sorce_url = "/uploads/" + req.params.id + "/playlist.m3u8";
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("movieInfo");
+        var query = {movie_id: req.params.id};
+        dbo.collection("movies").find(query).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+            res.render('class_page', {
+                role: false,
+                className: result[0].lecture_name,
+                videoTitle: result[0].lecture_name,
+                videoNumber: result[0].lecture_time,
+                place: sorce_url
             });
         });
     });
-    // var db = req.db;
-    // col.find({},{},function(e,docs){
-    //     res.render('userlist', {
-    //         "userlist" : docs
-    //     });
-    // });
 });
 
 app.get('/top', function (req, res) {
     res.sendfile('static/top.html');
 });
 
-io.sockets.on("connection", function(socket){
-	//connection stuff
-	memeConnections.push(socket);	
-				io.sockets.emit("new memer"); //checks if anyone is online
+// io.sockets.on("connection", function (socket) {
+//     //connection stuff
+//     memeConnections.push(socket);
+//     io.sockets.emit("new memer"); //checks if anyone is online
+//
+//     console.log("Memers connected: %s", memeConnections.length);
+//
+//
+//     // disconnection stuff
+//     socket.on("disconnect", function (data) {
+//
+//         memers.splice(memers.indexOf(socket.username), 1); //accessing the array memers
+//
+//         io.sockets.emit("memer left"); //checks if memer left
+//
+//         memeConnections.splice(memeConnections.indexOf(socket), 1);
+//         console.log("Memers disconnected: %s ", memeConnections.length);
+//     });
+//
+//     //send dem meme messages
+//     socket.on("send meme message", function (data) {
+//         console.log(data);// shows what the memers typed in console
+//         io.sockets.emit("new meme message", {msg: data});
+//
+//
+//     });
+//
+//
+// });
 
-	console.log("Memers connected: %s", memeConnections.length);
-	
-
-	
-	
-	// disconnection stuff
-	socket.on("disconnect", function(data){
-		
-		memers.splice(memers.indexOf(socket.username), 1); //accessing the array memers
-		
-						io.sockets.emit("memer left"); //checks if memer left
-
-	memeConnections.splice(memeConnections.indexOf(socket),1);
-	console.log("Memers disconnected: %s ", memeConnections.length);
-	});
-	
-	//send dem meme messages
-	socket.on("send meme message", function(data){ 
-		console.log(data);// shows what the memers typed in console
-		io.sockets.emit("new meme message", {msg: data});
-	
-	
-	});
-
-
-
-	});
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // console.log(typeof(JSON.stringify(file.originalname)));
+    destination: (req, file, cb) => {
+        // console.log(typeof(JSON.stringify(file.originalname)));
 
-    puid = new Puid();
-    puid = puid.generate();
-    var dir = 'uploads/' + puid
+        puid = new Puid();
+        puid = puid.generate();
+        var dir = 'uploads/' + puid
 
-    const connectOption = {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-    }
-    MongoClient.connect(url, connectOption, function(err, client) {
-        const col = client.db(dbName).collection('movies');
-        col.insert([{id:puid,place:dir}], {w:1}, function(err, result) {
-            test.equal(null, err);
+        const connectOption = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+        MongoClient.connect(url, connectOption, function (err, client) {
+            const col = client.db(dbName).collection('movies');
+            col.insert([{id: puid, place: dir}], {w: 1}, function (err, result) {
+                test.equal(null, err);
+            });
         });
-    });
-    // var dir = 'uploads/' + JSON.stringify(file.originalname);
-    var fs = require('fs');
-    if (!fs.existsSync(dir)) {
-      //create dir
-      var command = "mkdir -p "+dir;
-      execSync(command);
-      // fs.mkdir(dir);
-      // Makedir(dir);
-    };
-    cb(null, dir);
-  },
-  filename: (req, file, cb) => {
-      console.log(file);
-      cb(null, Date.now() + path.extname(file.originalname));
-  }
+        // var dir = 'uploads/' + JSON.stringify(file.originalname);
+        var fs = require('fs');
+        if (!fs.existsSync(dir)) {
+            //create dir
+            var command = "mkdir -p " + dir;
+            execSync(command);
+            // fs.mkdir(dir);
+            // Makedir(dir);
+        }
+        ;
+        cb(null, dir);
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
 });
-const upload = multer({ storage: storage});
+const upload = multer({storage: storage});
 
-app.use('/upload/', function(req, res, next) {
-  res.sendfile('static/upload.html'); // the uploaded file object
-  // res.send('<form action="/upload" method="POST" enctype="multipart/form-data">'+
-  // '<input name="movie" type="file"/>'+
-  // '<input type="submit" name="sub_buttono" value="Upload">'+'</form>');
-  console.log("get upload");
+app.use('/upload/', function (req, res, next) {
+    res.sendfile('static/upload.html'); // the uploaded file object
+    // res.send('<form action="/upload" method="POST" enctype="multipart/form-data">'+
+    // '<input name="movie" type="file"/>'+
+    // '<input type="submit" name="sub_buttono" value="Upload">'+'</form>');
+    console.log("get upload");
 });
 
 app.post('/upload', upload.single('movie'), function (req, res, next) {
-  console.log('post upload');
-  console.log(req.body);
-  console.log('req filepath:', req.file.path)
-  // var req = JSON.stringify(req);
-  // console.log('req.file:',req);
-  createSegments(__dirname, req);
-  console.log('createSegments END');
-  // console.log(`req.body: ${JSON.stringify(req.file)}`);
-  res.redirect("/userlist");
+    console.log('post upload');
+    console.log(req.body);
+    console.log('req filepath:', req.file.path)
+    // var req = JSON.stringify(req);
+    // console.log('req.file:',req);
+    createSegments(__dirname, req);
+    console.log('createSegments END');
+    // console.log(`req.body: ${JSON.stringify(req.file)}`);
+    res.redirect("/userlist");
 });
 
 
-
 //functions
-function createSegments(dir_name, req){
-  console.log('createSegments START');
-  var f_dir = dir_name + "/" + req.file.destination + "/";
-  var f_in_path = f_dir + req.file.filename;
-  var f_out_dir = f_dir + 'video%3d.ts" ' + f_dir + 'playlist.m3u8';
-  var command = 'ffmpeg -i ' + f_in_path + ' -codec copy -bsf:v h264_mp4toannexb -hls_time 9 -loglevel error -hls_list_size 0 -hls_segment_filename "'+ f_out_dir;
-  var result =  execSync(command);
-  // return 1
+function createSegments(dir_name, req) {
+    console.log('createSegments START');
+    var f_dir = dir_name + "/" + req.file.destination + "/";
+    var f_in_path = f_dir + req.file.filename;
+    var f_out_dir = f_dir + 'video%3d.ts" ' + f_dir + 'playlist.m3u8';
+    var command = 'ffmpeg -i ' + f_in_path + ' -codec copy -bsf:v h264_mp4toannexb -hls_time 9 -loglevel error -hls_list_size 0 -hls_segment_filename "' + f_out_dir;
+    var result = execSync(command);
+    // return 1
 };
-
 
 
 //app.listen(8080)
