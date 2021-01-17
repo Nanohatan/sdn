aeb6ad60b2752d2ea74e8a64df3ed75581596687
本間　野々花 <e185730@ie.u-ryukyu.ac.jp>
2021-01-17 16:17:38 +0900

    使ってない関数、変数を削除

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ var Puid = require('puid');
 var puid;
 
 const MongoClient = require('mongodb').MongoClient;
-const test = require('assert');
 const url = 'mongodb://localhost:27017';
-const dbName = 'test_kanon';
 const connectOption = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -116,43 +114,6 @@ io.on('connection', (socket) => {
     });
 });
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        // console.log(typeof(JSON.stringify(file.originalname)));
-
-        puid = new Puid();
-        puid = puid.generate();
-        var dir = 'uploads/' + puid
-
-        const connectOption = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
-        MongoClient.connect(url, connectOption, function (err, client) {
-            const col = client.db(dbName).collection('movies');
-            col.insert([{id: puid, place: dir}], {w: 1}, function (err, result) {
-                test.equal(null, err);
-            });
-        });
-        // var dir = 'uploads/' + JSON.stringify(file.originalname);
-        var fs = require('fs');
-        if (!fs.existsSync(dir)) {
-            //create dir
-            var command = "mkdir -p " + dir;
-            execSync(command);
-            // fs.mkdir(dir);
-            // Makedir(dir);
-        }
-        ;
-        cb(null, dir);
-    },
-    filename: (req, file, cb) => {
-        console.log(file);
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
-const upload = multer({storage: storage});
-
 app.use('/upload/', function (req, res, next) {
     res.sendfile('static/upload.html'); // the uploaded file object
     // res.send('<form action="/upload" method="POST" enctype="multipart/form-data">'+
@@ -161,29 +122,6 @@ app.use('/upload/', function (req, res, next) {
     console.log("get upload");
 });
 
-app.post('/upload', upload.single('movie'), function (req, res, next) {
-    console.log('post upload');
-    console.log(req.body);
-    console.log('req filepath:', req.file.path)
-    // var req = JSON.stringify(req);
-    // console.log('req.file:',req);
-    createSegments(__dirname, req);
-    console.log('createSegments END');
-    // console.log(`req.body: ${JSON.stringify(req.file)}`);
-    res.redirect("/userlist");
-});
 
 
-//functions
-function createSegments(dir_name, req) {
-    console.log('createSegments START');
-    var f_dir = dir_name + "/" + req.file.destination + "/";
-    var f_in_path = f_dir + req.file.filename;
-    var f_out_dir = f_dir + 'video%3d.ts" ' + f_dir + 'playlist.m3u8';
-    var command = 'ffmpeg -i ' + f_in_path + ' -codec copy -bsf:v h264_mp4toannexb -hls_time 9 -loglevel error -hls_list_size 0 -hls_segment_filename "' + f_out_dir;
-    var result = execSync(command);
-    // return 1
-};
 
-
-//app.listen(8080)
