10517ff6a3574fb2c15379de9587cabc1abdeb62
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2021-01-17 15:55:40 +0900

    add upload button in class_list_page.ejs

--- index.js
+++ index.js
@@ -151,8 +151,8 @@ const storage = multer.diskStorage({
 });
 const upload = multer({storage: storage});
 
-app.use('/upload/:class_name', function (req, res, next) {
-    res.render('uploads', {"class_name":req.body.class_name});
+app.use('/upload', function (req, res, next) {
+    res.render('uploads', {"class_name":req.query.class_name});
     // res.sendfile('static/upload.html'); // the uploaded file object
     // res.send('<form action="/upload" method="POST" enctype="multipart/form-data">'+
     // '<input name="movie" type="file"/>'+
