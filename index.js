f2fe2bb9711ea44b427d3e12c7ef9e231e51218d
kanon <yuriika.0000@gmail.com>
2021-01-18 00:53:03 +0900

    no message

--- index.js
+++ index.js
@@ -91,6 +91,8 @@ io.on('connection', (socket) => {
     });
 
     console.log('a user connected');
+    socket.join("first");
+    console.log(socket.rooms);
     socket.on('chat message', (msg, reaction, id, isParent, shiori_time, nowTime) => {
         puid = new Puid();
         puid = puid.generate();
