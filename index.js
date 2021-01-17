f9db7a08691c20f808d170718250d26bf56a1fef
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2021-01-17 18:52:44 +0900

    isTeacherを使った判定を修正した．

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ const upload = multer({storage: storage});
 
 app.use('/upload/', function (req, res, next) {
     if (!req.cookies.isTeacher){
-        res.redirect("/auth/login");
+        res.redirect("/home");
     };
     res.sendfile('static/upload.html'); // the uploaded file object
     // res.send('<form action="/upload" method="POST" enctype="multipart/form-data">'+
