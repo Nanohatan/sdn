17c743fc5b67f60b4443d35a6049709408c6afee
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2021-01-17 19:36:35 +0900

    update video_search.js

--- index.js
+++ index.js
@@ -120,11 +120,10 @@ io.on('connection', (socket) => {
 
 
 app.use('/upload/', function (req, res, next) {
-    res.sendfile('static/upload.html'); // the uploaded file object
     if (!req.cookies.isTeacher){
         res.redirect("/home");
     }else{
-        res.sendfile('static/upload.html'); // the uploaded file object
+        res.sendfile('upload', {req.query.class_name}); // the uploaded file object
     }
     // res.send('<form action="/upload" method="POST" enctype="multipart/form-data">'+
     // '<input name="movie" type="file"/>'+
