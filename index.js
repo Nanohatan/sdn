1342eba5ebf83c9419c72001ec4483b9efc4bd92
kanon <yuriika.0000@gmail.com>
2021-01-17 23:44:52 +0900

    no message

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ io.on('connection', (socket) => {
     var join_id;
     socket.on('join', function(id) {
         join_id = id;
+        console.log(join_id)
         socket.join(join_id);
         console.log(id+"に参加しました");
         console.log(socket.rooms);
