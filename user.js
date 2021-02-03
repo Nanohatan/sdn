17b1b6e2c433c8cb47b726a23c19577f1acbc238
本間　野々花 <e185730@ie.u-ryukyu.ac.jp>
2021-02-03 14:35:50 +0900

    add session check

--- user.js
+++ user.js
@@ -3,7 +3,12 @@ var router = express.Router()
 
 // define the home page route
 router.get('/', function (req, res) {
-  res.render('userPage', {role:req.session.user.isTeacher, userName : req.session.user.name, schedule:req.session.user.class });
+  var  sess = req.session.user;
+  if (sess){
+    res.render('userPage', {role:req.session.user.isTeacher, userName : req.session.user.name, schedule:req.session.user.class });
+  }else{
+    res.redirect('/auth/login');
+  }
 })
 
 module.exports = router
