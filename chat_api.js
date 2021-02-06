231a29a3f40c64bb877d55c69b7b03db6e20db47
本間　野々花 <e185730@ie.u-ryukyu.ac.jp>
2021-02-06 21:24:33 +0900

    add child api router

---
+++ chat_api.js
@@ -0,0 +1,40 @@
+var express = require('express')
+var router = express.Router()
+const { MongoClient } = require("mongodb");
+const uri = 'mongodb://localhost:27017' ;
+
+router.get('/api/get-thread/:id', async function (req, res) {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  var  sess = req.session.user;
+  if (sess){
+    try {
+      await client.connect();
+      const database = client.db('chatInfo');
+      const collection = database.collection('chats');
+      
+      var child_chats = await collection.aggregate([
+        { $match: {parent_id:req.params.id } }
+      ]);
+      var parent_chat = await collection.aggregate([
+        { $match: {its_id:req.params.id } }
+      ]);
+      child_chats = await child_chats.toArray();
+      parent_chat = await parent_chat.toArray();
+      res.render('chat_body/thread',{main:parent_chat[0],jsonAry:child_chats}, function (err, html) {
+        res.send(html)
+      })
+
+    }catch(err) {
+      console.log(err);
+    }
+    finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }else{
+    res.redirect('/auth/login');
+  }
+})
+
+
+module.exports = router
