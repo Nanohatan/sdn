b44f72af12e6ab7522477f6a9a9509c34eee6635
本間　野々花 <e185730@ie.u-ryukyu.ac.jp>
2021-02-06 22:12:46 +0900

    add api

--- chat_api.js
+++ chat_api.js
@@ -20,9 +20,8 @@ router.get('/get-thread/:id', async function (req, res) {
       ]);
       child_chats = await child_chats.toArray();
       parent_chat = await parent_chat.toArray();
-      res.render('chat_body/thread',{main:parent_chat[0],jsonAry:child_chats}, function (err, html) {
-        res.send(html)
-      })
+
+      res.render('chat_body/thread',{main:parent_chat[0],jsonAry:child_chats})
 
     }catch(err) {
       console.log(err);
@@ -35,6 +34,33 @@ router.get('/get-thread/:id', async function (req, res) {
     res.redirect('/auth/login');
   }
 })
+
+router.get('/back-thread/:id', async function (req, res) {
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+    var  sess = req.session.user;
+    if (sess){
+      try {
+        await client.connect();
+        const database = client.db('chatInfo');
+        const collection = database.collection('chats');
+        
+        var chats = await collection.aggregate([
+          { $match: {parent_id:req.params.id } }
+        ]);
+        chats = await chats.toArray();
+        res.render('chat_body/thread',{jsonAry:chats})
+  
+      }catch(err) {
+        console.log(err);
+      }
+      finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
+    }else{
+      res.redirect('/auth/login');
+    }
+  })
 
 
 module.exports = router
