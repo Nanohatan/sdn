e504e845abfdc785993c20c55aea8d38b31c1e67
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2021-02-08 17:32:03 +0900

    movie_upload.js いじった．動画投稿部分

--- movie_upload.js
+++ movie_upload.js
@@ -51,6 +51,7 @@ var url = "mongodb://localhost:27017/";
 router.get('/',function(req, res,next) {
   var user=req.session.user
   if (user && user.isTeacher){
+    console.log(req.session.user);
     res.render('uploads', {"class_name":req.query.class_name});
   }else{
     res.redirect("/user")
@@ -63,6 +64,9 @@ router.post('/', upload.single('movie'),function(req, res,next) {
     if (err) throw err;
     var dbo = db.db("movieInfo");
     var obj = {movie_id:req.file.movie_id, lecture_name:req.body.lecture_name, lecture_time:req.body.lecture_time,day:req.body.day ,period:req.body.period, class_name:req.body.class_name};
+    var obj = { name: req.body.class_name, lecture_year: req.body.lecture_year, semester: req.body.semester, teacher_id: "601a4634c1d7cdbb7336fcd2", day: req.body.day, timetable: req.body.timetable, is_archive: false, student_id: [], lectures: [ { time: lecture_time, video_id: req.file.movie_id, subtitle: req.body.lecture_name} ] };
+    // teacher_idはセッションからとる．
+    // var obj = { name: "コレクティブインテリジェンス", lecture_year: "2020", semester: "2", teacher_id: "601a4634c1d7cdbb7336fcd2", day: "5", timetable: ["1","2"], is_archive: false, student_id: ["5fd87210335edb82edcae756"], lectures: [ { time: "1", video_id: "", subtitle: "アリの行動と集合知"} ] };
     //var obj = { lecture_name:"Company Inc", lecture_time:0,day:"monday" ,period:2};
     dbo.collection("movies").insertOne(obj, function(err, res) {
       if (err) throw err;
