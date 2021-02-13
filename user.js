dac0aa6451a00474a7988a77369ece3a44c5e3dd
本間　野々花 <e185730@ie.u-ryukyu.ac.jp>
2021-02-14 00:25:13 +0900

    fix ユーザーのロールによるチャットの見える見えないを設定

--- user.js
+++ user.js
@@ -16,7 +16,7 @@ router.get('/', function (req, res) {
 router.get('/video/:id', async function (req, res) {
   const client = new MongoClient(uri, { useUnifiedTopology: true });
   var  sess = req.session.user;
-  if (sess){
+  if (sess && !sess.isTeacher){
     try {
       await client.connect();
       var sorce_url = "/uploads/" + req.params.id + "/playlist.m3u8";
@@ -26,6 +26,43 @@ router.get('/video/:id', async function (req, res) {
       
       var chats = await c_collection.aggregate([
         { $match: {parent_id:req.params.id } }
+      ]);
+      chats = await chats.toArray();
+
+      const m_database = client.db('movieInfo');
+      const m_collection = m_database.collection('movies');
+      const query = { movie_id:req.params.id };
+      const cursor = await m_collection.aggregate([
+        { $match: query }
+      ]);
+      const movie = await cursor.toArray();
+      
+      res.render('video_page', 
+      {role:req.session.user.isTeacher, 
+        className: movie[0].class_name,
+        videoTitle: movie[0].lecture_name,
+        videoNumber: movie[0].lecture_time,
+        place: sorce_url,
+        movie_id: req.params.id,
+        jsonAry:chats
+      });
+    }catch(err) {
+      console.log(err);
+    }
+    finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }else if (sess && sess.isTeacher){
+    try {
+      await client.connect();
+      var sorce_url = "/uploads/" + req.params.id + "/playlist.m3u8";
+      
+      const c_database = client.db('chatInfo');
+      const c_collection = c_database.collection('chats');
+      
+      var chats = await c_collection.aggregate([
+        { $match: {parent_id:req.params.id ,isWatchByTeacher:true} }
       ]);
       chats = await chats.toArray();
 
