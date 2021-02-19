1c125e257df4cfae5b45c3294ce9b54895066530
kanon <yuriika.0000@gmail.com>
2021-02-20 03:35:07 +0900

    no message

--- chat_api.js
+++ chat_api.js
@@ -149,7 +149,7 @@ router.post('/add_chat/:id',urlencodedParser, async function (req, res) {
       })
       console.log(doc)
       
-      res.send({test:req.params.id})
+      res.send({test:puid})
     }catch(err) {
       console.log(err);
     }
